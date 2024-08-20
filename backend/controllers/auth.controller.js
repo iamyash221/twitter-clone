8b5ac76dbede1833da80519861547319b9284fc2
Yash Chavda <yashchavda360@gmail.com>
2024-08-20 12:56:26 -0400

    Project Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "You hit Signup",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    data: "You hit Login",
+  });
+};
+export const logout = async (req, res) => {
+  res.json({
+    data: "You hit Logout",
+  });
+};
